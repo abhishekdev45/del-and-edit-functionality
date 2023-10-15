84fead8591a2f27e411fd80c1e93314733b6f4a2
abhishekdev45 <95223863+abhishekdev45@users.noreply.github.com>
2023-10-15 14:45:14 +0530

    made it persistant

--- app.js
+++ app.js
@@ -9,15 +9,18 @@ search.addEventListener('keyup' , filter);
 function addItem(e){
   e.preventDefault();
   var newItem = document.getElementById('item').value;
-  
+  newItem += " " + document.getElementById('desc').value;
   var li = document.createElement('li');
   li.className = 'list-group-item';
   li.appendChild(document.createTextNode(newItem));
   var deleteBtn = document.createElement('button');
   deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
   deleteBtn.appendChild(document.createTextNode('X'));
+ 
   li.appendChild(deleteBtn);
   itemList.appendChild(li);
+
+  updateLocalStorage();
 }
 
 function removeItem(e){
@@ -25,8 +28,19 @@ function removeItem(e){
     if(confirm('Are You Sure?')){
       var li = e.target.parentElement;
       itemList.removeChild(li);
+      updateLocalStorage();
     }
   }
+}
+
+function updateLocalStorage() {
+    var items = [];
+    var listItems = document.querySelectorAll('.list-group-item');
+    listItems.forEach(function(item) {
+      items.push(item.textContent);
+    });
+
+    localStorage.setItem('items', JSON.stringify(items));
 }
 
 function filter(e){
@@ -42,3 +56,24 @@ function filter(e){
         }
     })
 }
+
+function loadItemsFromLocalStorage() {
+    var items = JSON.parse(localStorage.getItem('items'));
+
+    if (items && items.length > 0) {
+      items.forEach(function (itemText) {
+        var li = document.createElement('li');
+        li.className = 'list-group-item';
+        li.appendChild(document.createTextNode(itemText));
+        var deleteBtn = document.createElement('button');
+        deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+        deleteBtn.appendChild(document.createTextNode('X'));
+
+        li.appendChild(deleteBtn);
+        itemList.appendChild(li);
+      });
+    }
+  }
+  
+window.addEventListener('load', loadItemsFromLocalStorage);
+  
